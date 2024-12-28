713dfface644d900519db2d0d53b67762e63c3a8
SpeedyTheXD <tranlinh4122005@gmail.com>
2024-12-28 15:39:21 +0700

    First commit

---
+++ ability_updater.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://pokemondb.net/ability"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+abilities = soup.find_all("a", class_="ent-name")
+ability_descriptions = soup.find_all("td", class_="cell-med-text")
+
+with open("ability.txt", "w") as file:
+    for ability, description in zip(abilities, ability_descriptions):
+        ability_name = ability.get_text()
+        ability_des = description.get_text().replace("Poké", "Poke")
+        file.write(f"{ability_name}: {ability_des}\n")
