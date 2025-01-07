4e79c648817bc9a812e541e740b6e7db4b47305f
SpeedyTheXD <tranlinh4122005@gmail.com>
2025-01-07 14:11:33 +0700

    organized information

--- source/ability_updater.py
+++ source/ability_updater.py
@@ -8,7 +8,7 @@ soup = BeautifulSoup(response.text, "html.parser")
 abilities = soup.find_all("a", class_="ent-name")
 ability_descriptions = soup.find_all("td", class_="cell-med-text")
 
-with open("ability.txt", "w") as file:
+with open("../data/ability.txt", "w") as file:
     for ability, description in zip(abilities, ability_descriptions):
         ability_name = ability.get_text()
         ability_des = description.get_text().replace("Poké", "Poke")
